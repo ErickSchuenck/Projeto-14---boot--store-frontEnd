e1095c0eccfaad034279788ac0d0fdb9a09d8cae
ErickSchuenck <esfmokdeci@gmail.com>
2022-05-10 21:30:50 -0300

    iniciando layout basico do login

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import axios from 'axios'
+import styled from 'styled-components';
+
 import { Link, useNavigate } from "react-router-dom";
 
 
 export default function LoginScreen() {
   return (
-    <div className='container'>
-      <img src={'../logo.png'} alt={'logo'}></img>
+    <Container>
+      <img
+        src='./assets/logo-without-background.png' alt='logo' />
       <h1>Flex Shoes</h1>
       <input
         type={'text'}
@@ -16,7 +19,50 @@ export default function LoginScreen() {
         type={'text'}
         placeholder={'senha'}
       />
-      <button>Entrar</button>
-    </div>
+      <button><p>Entrar</p></button>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  h1{
+    font-size: 45px;
+  }
+  img{
+    width: 100px;
+    height: auto;
+  }
+  input{
+    margin-bottom: 10px;
+    text-align: center;
+    cursor: pointer;
+    border: none;
+    width: 250px;
+    height: 26px;
+    border-radius: 8px;
+  }
+  input:focus {
+    outline: 0;
+  }
+  button{
+    border: none;
+    width: 250px;
+    height: 26px;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: .2s;
+    cursor: pointer;
+  }
+  button:hover{
+    transform: scale(1.05);
+    background-color: var(--highlightColor);
+  }
+`
